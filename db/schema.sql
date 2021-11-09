09e1a3585889e3c6f2a38f8b61d900c15d0f73d8
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-11-09 18:31:26 -0500

    updated id to increase without input value

--- db/schema.sql
+++ db/schema.sql
@@ -4,29 +4,26 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE departments (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30),
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id)
     REFERENCES departments(id)
     ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id),
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
     ON DELETE SET NULL,
