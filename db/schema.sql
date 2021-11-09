74553aac2bfc290cbf7e0efff54d1e0f8ff9d3a6
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-11-08 19:27:41 -0500

    fixed syntax so tables are successfully created

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,23 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
+CREATE TABLE departments (
+    id INT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
+);
+
 CREATE TABLE employees (
     id INT NOT NULL,
     first_name VARCHAR(30),
@@ -17,20 +34,3 @@ CREATE TABLE employees (
     REFERENCES employees(id)
     ON DELETE SET NULL
 );
-
-CREATE TABLE roles (
-    id INT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL
-);
-
-CREATE TABLE departments (
-    id INT NOT NULL,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
-)
