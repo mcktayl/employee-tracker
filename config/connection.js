0cb2ce941482a84b7cc126f5d37b14280eef90e8
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-11-08 19:13:59 -0500

    added connection to db

---
+++ config/connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: 'password',
+    database: 'employee_db'
+});
