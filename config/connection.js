00d219f89bba7fed21c7e2c7bf6a836d1ce9b184
McKenzie Cottrell <mckenziecottrell@gmail.com>
2021-11-08 19:28:02 -0500

    updated config settings

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,30 @@
+const express = require('express');
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3001,
-    user: 'root',
-    password: 'password',
-    database: 'employee_db'
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'employee_db'
+    },
+    console.log('Connected to the employee_db database.')
+);
+
+db.query('SELECT * FROM employees', function (err) {
+    console.log(err);
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+  
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
